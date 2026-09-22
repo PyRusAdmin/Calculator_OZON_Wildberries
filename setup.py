7f2c9fff24240adf24c22671ea63c9ead40ea34e
PyRusAdmin <zh.vitaliy92@yandex.ru>
2026-09-22 22:47:57 +0300

    Инициализация проекта

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='Калькулятор_OZON_WB',
+    version='0.1.0',
+    py_modules=['main'],
+    url='https://github.com/PyRusAdmin',
+    license='',
+    author='zhvit',
+    author_email='',
+    description='Калькулятор OZON / Wildberries'
+)
+
